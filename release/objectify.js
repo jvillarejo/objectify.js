8910e2beae2c32964fbf873060f7c4bc0d6dabfb
jvillarejo <arzivian87@gmail.com>
2013-07-06 16:40:29 -0300

    Added more methods

--- release/objectify.js
+++ release/objectify.js
@@ -1,7 +1,27 @@
-Array.prototype.first = function() { 
+//Array methods implementation
+var array = Array.prototype;
+
+array.first = function() { 
   if( this.length > 0) {
     return this[0];
   } else { 
     return null;
   }
-}
+};
+
+array.last = function() { 
+  if(this.length > 0) { 
+    return this[this.length -1];
+  } else {
+    return null;
+  }
+};
+
+array.any = function(condition) { 
+  for(var i=0; i<this.length; i++) {
+    if(condition(this[i])) {
+      return true;
+    }
+  }
+  return false;
+};
--- tests/array-test.js
+++ tests/array-test.js
@@ -3,9 +3,29 @@ var array = require("../release/objectify")
 
 describe('Array', function() { 
   describe('#first()', function(){
-    it('should return the first value of array or null if not present', function() {
+    it('should return the first value of array or null if there is nothing', function() {
       assert.equal(1, [1,2,3].first());
       assert.equal(null, [].first());
     });
   });
+
+  describe('#last', function() { 
+    it('should return the las value of array or null if there is nothing', function() { 
+      assert.equal(3, [1,2,3].last()); 
+      assert.equal(null, [].last());
+    });
+  });
+
+  describe('#any', function() { 
+    it('should return true if any satisfy the condition or false if none', function() {
+      var isEven = function(number) { 
+        return (number % 2) == 0;
+      }
+
+      assert.equal(true, [1,2,3].any(isEven));
+      assert.equal(false, [1,3,5].any(isEven));
+
+    });
+  });
+
 });
